1453bc6510021ccec14560e5d3614e0d06f81c7f
MajdHassoun <majd12hassoun@gmail.com>
2024-06-23 23:27:15 +0300

    add the project

---
+++ library task/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for
+from book import Book
+
+from storage import save_library, load_library
+
+app = Flask(__name__)
+library = load_library()
+
+
+@app.route('/')
+def index():
+    books = library.list_books()
+    return render_template('list_books.html', books=books)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        year = request.form['year']
+        genre = request.form['genre']
+        book = Book(title, author, year, genre)
+        library.add_book(book)
+        save_library(library)
+        return redirect(url_for('index'))
+    return render_template('add_book.html')
+
+
+@app.route('/edit/<title>', methods=['GET', 'POST'])
+def edit_book(title):
+    book = library.find_book(title)
+    if request.method == 'POST':
+        new_title = request.form['title']
+        new_author = request.form['author']
+        new_year = request.form['year']
+        new_genre = request.form['genre']
+        new_book = Book(new_title, new_author, new_year, new_genre)
+        library.edit_book(title, new_book)
+        save_library(library)
+        return redirect(url_for('index'))
+    return render_template('edit_book.html', book=book)
+
+
+@app.route('/delete/<title>', methods=['GET', 'POST'])
+def delete_book(title):
+    if request.method == 'POST':
+        library.delete_book(title)
+        save_library(library)
+        return redirect(url_for('index'))
+    book = library.find_book(title)
+    return render_template('delete_book.html', book=book)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ library task/storage.py
@@ -0,0 +1,18 @@
+import json
+from library import Library
+
+FILE_PATH = 'library.json'
+
+
+def save_library(library):
+    with open(FILE_PATH, 'w') as file:
+        json.dump(library.to_dict(), file)
+
+
+def load_library():
+    try:
+        with open(FILE_PATH, 'r') as file:
+            data = json.load(file)
+            return Library.from_dict(data)
+    except FileNotFoundError:
+        return Library()
