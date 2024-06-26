4c0947f662144ffa7c7a80a2b3e91690628177ae
MajdHassoun <majd12hassoun@gmail.com>
2024-06-26 11:38:27 +0300

    add file

--- unit testing lesson/test_math_funcction.py
+++ unit testing lesson/test_math_funcction.py
@@ -35,5 +35,40 @@ class TestMathFunction(unittest.TestCase):
         self.assertEqual(0, modulo_func(4), "The function not working well.")
         # self.assertEqual(Expected, Actual, msg)
 
-
     def test_sum_function_result(self):
+        self.assertEqual(12, sum_function(8, 4), "the sum function isn't working")
+
+    def test_sum_function_oneNeg(self):
+        self.assertEqual(-10, sum_function(-13, 3), "the sum function isn't working")
+
+    def test_sum_function_twoNeg(self):
+        self.assertEqual(-12, sum_function(-8, -4), "the sum function isn't working")
+
+    def test_sum_function_twoZero(self):
+        self.assertEqual(0, sum_function(0, 0), "the sum function isn't working")
+
+    def test_sum_function_numZero(self):
+        self.assertEqual(5, sum_function(5, 0), "the sum function isn't working")
+
+    def test_sum_function_char(self):
+        self.assertEqual(5, sum_function(5, 'f'), "wrong input has been inserted")
+
+    def test_sub_function_result(self):
+        self.assertEqual(8, sub_function(12, 4), "the sum function isn't working")
+
+    def test_sub_function_Neg(self):
+        self.assertEqual(-1, sub_function(4, 5), "the sum function isn't working")
+
+    def test_sub_function_twoNeg(self):
+        self.assertEqual(-4, sub_function(-8, -4), "the sum function isn't working")
+
+    def test_sub_function_twoZero(self):
+        self.assertEqual(0, sub_function(0, 0), "the sum function isn't working")
+
+    def test_sub_function_numZero(self):
+        self.assertEqual(5, sub_function(5, 0), "the sum function isn't working")
+
+    def test_sub_function_char(self):
+        self.assertEqual(5, sub_function(5, 'f'), "wrong input has been inserted")
+
+# should cover the other funcs
