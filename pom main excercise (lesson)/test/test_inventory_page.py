bcb2297ea71de3f5c9d7a746bda4a219fa99eb2d
MajdHassoun <majd12hassoun@gmail.com>
2024-07-04 10:04:28 +0300

    uploading unfinished file

--- pom main excercise (lesson)/test/test_inventory_page.py
+++ pom main excercise (lesson)/test/test_inventory_page.py
@@ -3,15 +3,23 @@ import unittest
 
 from selenium import webdriver
 
-from infra.browser_wrapper import BrowserWrapper
+# from infra.browser_wrapper import BrowserWrapper
 from logic.inventory_page import InventoryPage
 from logic.login_page import LoginPage
+from logic.shopping_cart import ShoppingCart
 
 
 class TestInventoryPage(unittest.TestCase):
-    driver = webdriver.Chrome()
-    driver.get("https://www.saucedemo.com/")
-    login_page = LoginPage(driver)
-    login_page.login_flow("standard_user", "secret_sauce")
-    inventory_page = InventoryPage(driver)
-    InventoryPage.add_backpack_to_cart()
+    def test_stam_test(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.saucedemo.com/")
+        login_page = LoginPage(driver)
+        login_page.login_flow("standard_user", "secret_sauce")
+        time.sleep(2)
+        inventory_page = InventoryPage(driver)
+        inventory_page.add_backpack_to_cart()
+        time.sleep(2)
+        cart_page = ShoppingCart(driver)
+        cart_page.count_items_in_cart()
+        time.sleep(2)
+        time.sleep(3)
--- pom main excercise (lesson)/test/test_login_page.py
+++ pom main excercise (lesson)/test/test_login_page.py
@@ -4,16 +4,20 @@ import unittest
 from selenium import webdriver
 
 from infra.browser_wrapper import BrowserWrapper
+from infra.config_provider import ConfigProvider
+from logic.inventory_page import InventoryPage
 from logic.login_page import LoginPage
 
 
 class TestLoginPage(unittest.TestCase):
 
     def test_login(self):
-        driver = webdriver.Chrome()
+        config = ConfigProvider.load_from_file('../config.json')
+        driver = BrowserWrapper().get_driver(config["base_url"])
         driver.get("https://www.saucedemo.com/")
         login_page = LoginPage(driver)
         login_page.login_flow("standard_user", "secret_sauce")
+        inventory_page = InventoryPage(driver)
         time.sleep(2)
 
         
